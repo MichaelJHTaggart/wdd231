b899cb2c7f68aaa22f72edb03bf2980581567279
Michael Taggart <mtaggarthawk@gmail.com>
2026-01-21 21:08:55 -0700

    week 2 assignment start

---
+++ chamber/scripts/navigation.js
@@ -0,0 +1,9 @@
+// Use JavaScript to support the responsive menu.
+const navbutton = document.querySelector('#ham-btn');
+const navlinks = document.querySelector('#nav-bar')
+
+
+navbutton.addEventListener('click', () => {
+    navbutton.classList.toggle('show');
+    navlinks.classList.toggle('show');
+})
