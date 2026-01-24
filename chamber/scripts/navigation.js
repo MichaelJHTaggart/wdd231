ad12cc8b49e168c26966e7bc2c259ea1087ca1da
Michael Taggart <mtaggarthawk@gmail.com>
2026-01-24 15:26:08 -0700

    week 2

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -7,3 +7,36 @@ navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
     navlinks.classList.toggle('show');
 })
+
+const gridButton = document.querySelector('#directory-grid');
+const listButton = document.querySelector('#directory-list');
+const cardsWrapper = document.querySelector('#cards')
+const card = document.querySelectorAll('.card');
+
+console.log("card", card)
+
+gridButton.addEventListener('click', ()=>{  
+    // remove background
+    listButton.classList.remove('grid-active')
+    // add background
+    gridButton.classList.add('grid-active')
+    
+    // update styling
+    cardsWrapper.classList.add('grid')
+    cardsWrapper.classList.remove('list')
+
+    card.classList.remove('list-card')
+})
+
+listButton.addEventListener('click', ()=>{
+    // add background
+    listButton.classList.add('grid-active')
+    // remove background
+    gridButton.classList.remove('grid-active')
+
+    // update styling
+    cardsWrapper.classList.remove('grid')
+    cardsWrapper.classList.add('list')
+
+    card.classList.add('list-card')
+})
