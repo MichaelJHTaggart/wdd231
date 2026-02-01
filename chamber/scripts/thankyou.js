d40e7f32dcc421bdc1dc19beee6a719cfc7d9a26
Michael Taggart <mtaggarthawk@gmail.com>
2026-02-01 16:23:15 -0700

    week 4 assignment - finished everything but modals and animation

---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,15 @@
+const getString = window.location.search;
+
+console.log(getString);
+
+const myInfo = new URLSearchParams(window.location.search);
+
+console.log(myInfo.get('first'));
+
+document.querySelector('#form-submission-info').innerHTML = `
+<p>Thank you <strong>${myInfo.get('firstName')} ${myInfo.get('lastName')}</strong>,</p>
+<p>Your Business/Organization, <strong>${myInfo.get('businessName')}</strong> requested the <strong>${myInfo.get('membership-level')}</strong> membership on <strong>${new Date(myInfo.get('timestamp')).toLocaleString('en-US', {  year: 'numeric', month: 'short', day: 'numeric'})}</strong></p>
+<p><p>Phone: <strong>${myInfo.get('phone')}</strong> </p>
+<p>Email: <strong>${myInfo.get('email')}</strong></p>
+<p>We'll be in contact shortly!</p>
+`
